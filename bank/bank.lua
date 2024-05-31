e7c0d5ecacd2739699a62e5ba9546276504a21db
Ivan Duran <iamivanduran02@gmail.com>
2024-05-31 00:13:19 -0400

    comments

--- bank/bank.lua
+++ bank/bank.lua
@@ -1,5 +1,8 @@
+-- Bank Table
 Bank = { running = false, balance = 0 }
 
+--[[ Still thinking about how I want to construct
+the event loop, this might change ]]
 function Bank:start()
     Bank.running = true
 end
@@ -8,6 +11,9 @@ function Bank:stop()
     Bank.running = false
 end
 
+------------------------------
+
+-- Bank functionality, Ex. Withdraw, Deposit, Transfer, etc...
 function Bank:deposit(amount)
     print(string.format("Attempting to deposit $%d...", amount))
     Bank.balance = Bank.balance + amount
@@ -16,6 +22,7 @@ end
 
 function Bank:withdraw(amount)
     print(string.format("Attempting to withdraw $%d...", amount))
+    -- Check if there are sufficient funds in the account
     if Bank.balance > amount then
         Bank.balance = Bank.balance - amount
         print(string.format("Successfully withdrew $%d\nNew Balance = $%d", amount, Bank.balance))
